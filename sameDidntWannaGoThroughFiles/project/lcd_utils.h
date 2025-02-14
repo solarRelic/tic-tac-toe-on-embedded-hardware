1bacb3e945cb0dc6156ec17ba62c4614697f08e8
solarRelic <rektz@list.ru>
2025-02-14 23:10:49 +0100

    removed sameDidn

--- sameDidntWannaGoThroughFiles/project/lcd_utils.h
+++
@@ -1,3 +0,0 @@
-// void display_black(unsigned char *parlcd_mem_base, unsigned short *fb);
-void display_reveal(unsigned char *parlcd_mem_base, unsigned short *fb);
-void display_black(unsigned char *parlcd_mem_base, unsigned short *fb);
--- sameDidntWannaGoThroughFiles/project/pvpc_tic_tac_toe.h
+++
@@ -1,7 +0,0 @@
-#ifndef PVPC_TIC_TAC_TOE_H
-#define PVPC_TIC_TAC_TOE_H
-#include "font_types.h"
-
-void vs_pc(uint8_t *mem_base, unsigned char *parlcd_mem_base, font_descriptor_t *fdes, unsigned short *fb, volatile uint32_t *rgb1, volatile uint32_t *rgb2);
-
-#endif
